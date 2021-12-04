b7c459b23d1e770f31d08ae4cb25372dcaef03df
Jamile Lima <jamilelima@protonmail.com>
2021-12-04 18:41:39 -0300

    Initial Commit

---
+++ Heroes/HeroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroesApp.swift
+//  Heroes
+//
+//  Created by Jamile Bastos on 04/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HeroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
